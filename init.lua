b057062dbd65ea603ea6f0418c9e1f813fee67fd
Dzikry <dzikrynp14@gmail.com>
2025-09-21 10:04:29 +0700

    docs: finish read

--- init.lua
+++ init.lua
@@ -540,7 +540,6 @@ require('lazy').setup({
         end,
       })
 
-      -- continue here
       -- Diagnostic Config
       -- See :help vim.diagnostic.Opts
       vim.diagnostic.config {
@@ -587,7 +586,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
